10c71c954d5a3f7bdc51b8adc10d10dc114bf2df
unknown <rizwankazi661@gmail.com>
2025-01-12 18:47:38 +0530

    feat: added react material UI

--- frontend/src/Pages/Chat/Chat.component.tsx
+++ frontend/src/Pages/Chat/Chat.component.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@mui/material'
 import React from 'react'
 
 const ChatComponent = () => {
     return (
         <div>
             Chat Works
+            <Button variant="contained">Contained</Button>
         </div>
     )
 }
