90a6166a9933babeea716e9ecc7c2869514f7ca6
unknown <rizwankazi661@gmail.com>
2025-01-28 22:32:34 +0530

    feat: Added Auth Screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import SignIn from './Pages/Auth/SignIn/SignIn';
 import SignUp from './Pages/Auth/SignUp/SignUp';
 import Chat from './Pages/Chat/Chat';
+import Auth from './Pages/Auth/Auth';
 import './App.scss';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/auth/signin" element={<SignIn />} />
-        <Route path="/auth/signup" element={<SignUp />} />
+        <Route path="/auth" element={<Auth />} />
         <Route path="/chat" element={<Chat />} />
       </Routes>
     </BrowserRouter>
--- frontend/src/Pages/Auth/Auth.tsx
+++ frontend/src/Pages/Auth/Auth.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import './Auth.styles.scss';
+import logo from '../../assets/logo.png'
 
 const Auth = () => {
     return (
-        <div>
-            Auth Works
+        <div className='main_container'>
+            <div className="image_container"></div>
+            <div className="form_container">
+                <div className="title_and_icon_container">
+                    <div className="img">
+                        <img src={logo} alt="Talk-up" />
+                    </div>
+                    <div className="title">
+                        Talk-Up
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
