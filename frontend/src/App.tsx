72f8eccc5acf7faadc83b3989170aee93dc1a9b2
unknown <rizwankazi661@gmail.com>
2025-01-29 22:46:46 +0530

    feat: Added initial screens

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Auth from './Pages/Auth/Auth';
 
 function App() {
   return (
     <div className="App">
-
+      <Auth></Auth>
     </div>
   );
 }
---
+++ frontend/src/Pages/Auth/Auth.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Auth.module.css'
+
+export default function Auth() {
+    return (
+        <div className={styles.main_container}>
+            Auth Works
+        </div>
+    )
+}
