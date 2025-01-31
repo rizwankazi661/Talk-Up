8bb7549d0e1eb625cde047bd40c63e375f16c4d0
unknown <rizwankazi661@gmail.com>
2025-01-31 06:35:19 +0530

    feat: added default route as Auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Auth from './pages/Auth/Auth';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
+          <Route path="/" element={<Navigate to="/auth" />} />
           <Route path="/auth" element={<Auth />} />
         </Routes>
       </div>
