3f22a7f56cbd6d26fbed82d4e448f611f67291ca
unknown <rizwankazi661@gmail.com>
2025-02-09 19:09:43 +0530

    feat: Navbar styles updated

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -2,19 +2,28 @@ import React from 'react';
 import styles from './Navbar.module.css'
 import texts from '../../constants/en.json'
 import notificationIcon from '../../assets/svg/notification-icon.svg'
-import Input from '../common/Input/Input';
+import searchIcon from '../../assets/svg/search-icon.svg'
+import profileIcon from '../../assets/svg/profile-icon.svg'
+import arrowDownIcon from '../../assets/svg/arrow-down-icon.svg'
 
 function Navbar() {
     return (
         <div className={styles.main_container}>
             <div className={styles.left_container}>
-                <img className={styles.notification_icon} src={notificationIcon} alt="" />
-                <img className={styles.notification_icon} src={notificationIcon} alt="" />
+                <div className={styles.search_container}>
+                    <img className={styles.search_icon} src={searchIcon} alt="" />
+                    <div className={styles.search_title}>Search User</div>
+                </div>
             </div>
             <div className={styles.title}>{texts.appTitle}</div>
             <div className={styles.right_container}>
-                <img className={styles.notification_icon} src={notificationIcon} alt="" />
-                <img className={styles.notification_icon} src={notificationIcon} alt="" />
+                <div className={styles.notification_container}>
+                    <img className={styles.notification_icon} src={notificationIcon} alt="" />
+                </div>
+                <div className={styles.profile_container}>
+                    <img className={styles.profile_icon} src={profileIcon} alt="" />
+                    <img className={styles.arrow_icon} src={arrowDownIcon} alt="" />
+                </div>
             </div>
         </div>
     )
